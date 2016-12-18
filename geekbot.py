3f9ccd9df7b87f05890dbdafedad5dbca61788f0
Mayank Agrawal <agarwalm214@gmail.com>
2016-12-18 23:01:10 +0530

    add handler for message

--- geekbot.py
+++ geekbot.py
@@ -6,10 +6,21 @@ BOT_ID = os.environ.get("BOT_ID")
 
 # constants
 AT_BOT = "<@" + BOT_ID + ">"
-EXAMPLE_COMMAND = "do"
 
-# instantiate Slack & Twilio clients
 slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+
+def parse_slack_output(message):
+    msg_list = message
+    if msg_list and len(msg_list) > 0:
+        for msg in msg_list:
+            if msg and 'text' in msg and AT_BOT in output['text']:
+                return output['text'].split(AT_BOT)[1].strip().lower,
+                output['channel']
+    return None, None
+
+def handle_command(command, channel):
+    slack_client.api_call("chat.postMessage", channel=channel,
+                              text="Hello", as_user=True)
 
 if __name__ == "__main__":
     READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
